2c455965dd42a3cae62eefa00813183897dc0085
Gérald Pereira <gerald.pereira@octoperf.com>
2020-04-09 17:42:15 +0200

    Refactoring

---
+++ backend/commons/analysis/analysis-task-events-watcher/src/test/java/com/kraken/analysis/task/events/watcher/TaskEventsWatcherTest.java
@@ -0,0 +1,43 @@
+package com.kraken.analysis.task.events.watcher;
+
+import com.kraken.runtime.event.TaskCreatedEventTest;
+import com.kraken.runtime.event.client.api.RuntimeEventClient;
+import com.kraken.runtime.event.client.api.RuntimeEventClientFactory;
+import com.kraken.security.authentication.api.AuthenticationMode;
+import com.kraken.tools.event.bus.EventBus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskEventsWatcherTest {
+
+  @Mock
+  RuntimeEventClientFactory clientFactory;
+
+  @Mock
+  RuntimeEventClient client;
+  @Mock
+  EventBus eventBus;
+
+  TaskEventsWatcher watcher;
+
+  @Before
+  public void setUp() {
+    watcher = new TaskEventsWatcher(clientFactory, eventBus);
+  }
+
+  @Test
+  public void shouldWatch(){
+    given(clientFactory.create(AuthenticationMode.SERVICE_ACCOUNT)).willReturn(client);
+    given(client.events()).willReturn(Flux.just(TaskCreatedEventTest.TASK_CREATED_EVENT));
+    watcher.watch();
+    verify(eventBus).publish(TaskCreatedEventTest.TASK_CREATED_EVENT);
+  }
+}
